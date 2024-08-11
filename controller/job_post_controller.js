57bb2363e7ce83b2d0e27c4be25d1ddc1f802112
Ayushman Gupta <guptaayushman24@gmail.com>
2024-08-11 21:42:51 +0530

    Same Company cannot post same role

--- controller/job_post_controller.js
+++ controller/job_post_controller.js
@@ -4,8 +4,28 @@ async function jobpostfunction(req,res){
         const body = req.body;
         const {companyname,role} = req.body;
         // role frontend
-        const databasedata = await job_post.findOne({companyname:companyname});
+        const databasedata = await job_post.find({companyname:companyname});
+        //  console.log(databasedata.length)
         if (!databasedata){
+            const data = await job_post.create({
+                role:body.role,
+                companyname:body.companyname,
+                duration:body.duration,
+             
+                jobtype:body.jobtype,
+                location:body.location,
+               
+                userrole:body.userrole,
+                stipend:body.stipend,
+               })
+               return res.json({data});
+        }
+        else{
+            for (let i=0;i<databasedata.length;i++){
+                if (databasedata[i].role==role){
+                    return res.json({'msg':'Company already posted that role'});
+                }
+            }
             const data = await job_post.create({
                 role:body.role,
                 companyname:body.companyname,
@@ -18,29 +38,8 @@ async function jobpostfunction(req,res){
                 stipend:body.stipend,
                })
                return res.json({data});
+            
         }
-        if (databasedata.companyname!=companyname || databasedata.role!=role){
-            console.log("From db",databasedata.companyname);
-            console.log("From db",databasedata.role);
-            console.log("From user",companyname);
-            console.log("From user",role);
-        const data = await job_post.create({
-         role:body.role,
-         companyname:body.companyname,
-         duration:body.duration,
-      
-         jobtype:body.jobtype,
-         location:body.location,
-        
-         userrole:body.userrole,
-         
-         stipend:body.stipend,
-        })
-        return res.json({data});
-    }
-    else{
-        return res.json({'msg':'Company Alerady posted that role'});
-    }
         
     }
     catch(err){
@@ -52,3 +51,28 @@ async function jobpostfunction(req,res){
 module.exports = {
     jobpostfunction
 }
+
+
+
+ //     if (databasedata.companyname!=companyname || databasedata.role!=role){
+    //         console.log("From db",databasedata.companyname);
+    //         console.log("From db",databasedata.role);
+    //         console.log("From user",companyname);
+    //         console.log("From user",role);
+    //     const data = await job_post.create({
+    //      role:body.role,
+    //      companyname:body.companyname,
+    //      duration:body.duration,
+      
+    //      jobtype:body.jobtype,
+    //      location:body.location,
+        
+    //      userrole:body.userrole,
+         
+    //      stipend:body.stipend,
+    //     })
+    //     return res.json({data});
+    // }
+    // else{
+    //     return res.json({'msg':'Company Alerady posted that role'});
+    // }
